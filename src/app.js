6e7b2e18d8d27d45719b8783b5c6251ccf6895e4
Georgi <georgi.khorosh@gmail.com>
2023-07-02 15:02:08 +0300

    added table filling

--- src/app.js
+++ src/app.js
@@ -1,13 +1,46 @@
-import { load } from "parcel-bundler/lib/utils/config";
 
 export class App {
     static async loadData() {
         const response = await fetch("https://statfin.stat.fi/PxWeb/sq/4e244893-7761-4c4f-8e55-7a8d41d86eff");
         const jsonData = await response.json();
-        console.log(jsonData);
+        const area = jsonData.dataset.dimension.Alue.category.label;
+        const list = [];
+        const regNames = Object.values(area);
+        const popNumbers = jsonData.dataset.value; 
+        
+        for (let i = 0; i < regNames.length; i++) {
+            let population = 0;
+            if (i < popNumbers.length) {
+                population = popNumbers[i];
+            }
+            list.push({
+                regionName:regNames[i], 
+                population:population,
+            });
+        }
+
+        return list;
     }
-    
-    static run() {
-        this.loadData();
+
+    static createCell(content) {
+        const cell = document.createElement("td");
+        cell.innerText = content;
+        return cell;
+    }
+
+    static fillTable(dataList) {
+        const table = document.getElementById("statistic-table");
+        dataList.forEach(element => {
+            const tr = document.createElement("tr");
+            tr.appendChild(this.createCell(element.regionName));
+            tr.appendChild(this.createCell(element.population));
+            table.appendChild(tr);
+        });
+    } 
+
+    static async run() {
+        const dataList = await this.loadData();
+        console.log(dataList);
+        this.fillTable(dataList);
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import { load } from "parcel-bundler/lib/utils/config";
+import { App } from "./app";
 import "./styles.css";
-import { App } from "./app.js";
 
 App.run();
+console.log("qwerasdfzxcv")
+//fetch("https://statfin.stat.fi/PxWeb/sq/4e244893-7761-4c4f-8e55-7a8d41d86eff");
